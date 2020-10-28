52f6711969fc95b835b2d8ed266163157787723e
SwissalpS <Luke@SwissalpS.ws>
2020-10-29 00:17:02 +0100

    sketch of how we could implement more commands

--- lua/controller/digilines.lua
+++ lua/controller/digilines.lua
@@ -15,16 +15,49 @@ function drawers.controller.on_digiline_receive(pos_controller, _, channel, msg)
 	if channel ~= meta:get_string('channel') then
 		return
 	end
-	-- msg can be string 'default:cobble 34'
-	-- or table { name = 'default:cobble', count = 34 }
-	local stack = ItemStack(msg)
-	local taken_stack = drawers.controller.take(pos_controller, stack)
-	if 0 >= taken_stack:get_count() then
+	local message = {}
+	local msg_type = type(msg)
+	if 'string' == msg_type
+		or ('table' == msg_type and (not msg.command))
+	then
+		message = { command = 'take', stack = msg }
+	elseif 'table' == msg_type and msg.command then
+		message = msg
+	else
 		return
 	end
+	local command = message.command
+	if 'take' == command then
+		if not message.stack then
+			return
+		end
+		-- message.stack can be string 'default:cobble 34'
+		-- or table { name = 'default:cobble', count = 34 }
+		local stack = ItemStack(message.stack)
+		local taken_stack = drawers.controller.take(pos_controller, stack)
+print('digiline, got stack: ', taken_stack:to_string())
+		if 0 >= taken_stack:get_count() then
+			return
+		end
 
-	local dir = minetest.facedir_to_dir(minetest.get_node(pos_controller).param2)
-	local stack_string = taken_stack:to_string()
-	pipeworks.tube_inject_item(pos_controller, pos_controller, dir, stack_string)
+		local dir = minetest.facedir_to_dir(minetest.get_node(pos_controller).param2)
+		local stack_string = taken_stack:to_string()
+		pipeworks.tube_inject_item(pos_controller, pos_controller, dir, stack_string)
+		return
+	end
+--[[
+	if 'has' == command then
+		if not message.name then
+			return
+		end
+		local meta = minetest.get_meta(pos_controller)
+		local net_index = minetest.deserialize(meta:get_string('net_index'))
+		local item_index = net_index[message.name]
+		if not item_index then
+			return false -- actually send digiline message
+		end
+		-- check if it really does have some in that drawer
+	end
+--]]
 end -- drawers.controller.on_digiline_receive
 
